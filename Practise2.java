4ea987bbc59bcd511becfb22a1b029da06e615e4
Kishore-Clayfin <110982304+Kishore-Clayfin@users.noreply.github.com>
2022-08-29 18:21:39 +0530

    29/08/2022
    same method in class & interface which will it print

---
+++ Practise2.java
@@ -0,0 +1,29 @@
+package taskPrograms;
+
+public class Practise2  extends A4 implements I
+{
+	@Override
+	public void get() 
+	{
+		System.out.println("Hiim I");
+	}
+	public static void main(String[] args)
+	{
+		Practise2 p=new Practise2();
+		p.get();
+	}
+}
+
+class A4 
+{
+	public void get() 
+	{
+		System.out.println("Hii C");
+	}
+}
+
+interface I
+{
+	public void get();
+}
+
