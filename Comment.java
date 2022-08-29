c3bee38893c04317b0dc4eeb033bb06ac53e0c18
Kishore-Clayfin <110982304+Kishore-Clayfin@users.noreply.github.com>
2022-08-29 14:01:13 +0530

    29/08/2022

---
+++ Comment.java
@@ -0,0 +1,16 @@
+package taskPrograms;
+
+public class Comment {
+	public static void main(String[] args) {
+
+		// \u000d System.out.println("This  commented line will be printed");
+		System.out.println("");
+
+		//Without Semicolon
+		if (System.out.printf("Hello world!")==null) 
+		{
+
+		}
+
+	}
+}
